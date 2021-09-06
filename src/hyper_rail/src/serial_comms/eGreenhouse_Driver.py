43212626d8498df80b6760c68df77aa8408e0e9d
Will Richards <jwilt9000@gmail.com>
2021-09-06 10:07:24 -0700

    Completed functionality for parsing JSON. Then publishing it to the sensor reading topic

--- src/hyper_rail/src/serial_comms/eGreenhouse_Driver.py
+++ src/hyper_rail/src/serial_comms/eGreenhouse_Driver.py
@@ -1,6 +1,10 @@
 """Handles reading and parsing JSON data returned from the eGreenhouse package into a ROS message format"""
 
 import serial
+import json
+from time import sleep
+
+from hyper_rail.msg import GreenhouseSensorReadings
 
 class GreenhouseDriver():
     def __init__(self, baud_rate, device_path) -> None:
@@ -8,7 +12,32 @@ class GreenhouseDriver():
         Construct a new eGreenhouse Driver
 
         Keyword arguments:
-            baud_rate - Rate at which traffic is transimitted over the serial connection
+            baud_rate - Rate at which traffic is transimitted over the serial connection\n
             device_path - /dev/ path to the serial connection point
         """
-        self.serial = serial.Serial(device_path, baud_rate)
+        self.serial: serial.Serial = serial.Serial(device_path, baud_rate)
+
+    def poll_data(self) -> GreenhouseSensorReadings:
+        """Poll the current data readings from the sensor package"""
+
+        # Write a single byte to the serial buffer, this tells the end effector to send us new sensor data
+        self.serial.write(b"A")
+
+        # Wait a tiny bit (10ms)
+        sleep(0.01)
+
+        # Read 223 bytes from the serial port (size of the json data) and convert it back into a string which is then parsed into a useable python format
+        json_data = json.loads(self.serial.read(223).decode('utf-8'))
+
+        return GreenhouseSensorReadings(
+            # Pull the time and value for the CO2 sensor
+            reading_time=json_data[0]["time"],
+            co2 = json_data[0]["value"],
+
+            # Pull the temp and humidity from the SHT31
+            temperature = json_data[1]["temperature"],
+            humidity = json_data[1]["humidity"],
+
+            # Pull the light levels from the TSL2591
+            luminosity = json_data[2]["value"]
+            )
