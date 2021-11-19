78283536e4e61770d397ca4460a0e9af17b7d13c
Kelley Reynolds <kelley@insidesystems.net>
2021-11-18 22:39:27 -0500

    Basic analysis scaffold

--- WebUI/config/routes.rb
+++ WebUI/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :analyses
   resource :session, only: [:new, :create, :destroy]
   resources :programs
+  resources :analyses
 
   get '/dashboard', to: 'dashboard#index', as: :dashboard
   root to: 'dashboard#index'
