d97516e31a12a6c9f919f67e901322086821c02a
Kelley Reynolds <kelley@insidesystems.net>
2021-11-22 14:40:35 -0500

    Add basic settings page

--- WebUI/config/routes.rb
+++ WebUI/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :programs
   resources :analyses
 
+  get '/settings', to: 'settings#index', as: :settings
+  post '/settings', to: 'settings#update', as: :update_settings
+
   get '/dashboard', to: 'dashboard#index', as: :dashboard
   root to: 'dashboard#index'
 end
