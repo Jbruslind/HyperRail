cbfc35b6b44dbaf872e4ae8bdb094e8e0873c070
Kelley Reynolds <kelley@insidesystems.net>
2021-11-27 20:40:33 -0500

    Add most recent measurements to the status panel

--- WebUI/app/models/area_of_interest.rb
+++ WebUI/app/models/area_of_interest.rb
@@ -1,3 +1,6 @@
 class AreaOfInterest < ApplicationRecord
   belongs_to :analysis
+
+  validates :x, :y, :width, :height, presence: true, numericality: true
+  validates :width, :height, numericality: { greater_than: 0 }
 end
